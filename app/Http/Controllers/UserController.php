5827d3ff0851162641033e69b85ad2b6b95b2e6e
AliciaPeris <aliciadelacruzperis@gmail.com>
2017-05-18 21:47:36 +0200

    Crear userController / directorio de vistas / vista create (adminuser/create) ruta para verlo localhost:8000/adminusuarios/create

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace moviexpert\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use moviexpert\Http\Requests;
+use moviexpert\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    //
+    public function index(){
+
+    }
+    public function create(){
+          return view('adminuser.create');
+    }
+    public function store(){
+
+    }
+    public function show($id){
+
+    }
+    public function edit($id){
+
+    }
+    public function update($id){
+
+    }
+    public function destroy($id){
+
+    }
+}
---
+++ resources/views/adminuser/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.admin')
+	@section('content')
+  {!!Form::open()!!}
+  <h1 class="textoMarron text-center">Formulario de registro de usuario</h1>
+  <div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1">
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-envelope"></span>
+        {!!Form::text('email',null,['class'=>'form-control','placeholder'=>'Ingrese el email'])!!}
+      </div>
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-certificate"></span>
+        {!!Form::password('password',['class'=>'form-control','placeholder'=>'Ingrese la contraseña'])!!}
+      </div>
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-font"></span>
+        {!!Form::text('nombre',null,['class'=>'form-control','placeholder'=>'Ingrese el nombre'])!!}
+      </div>
+    <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-bold"></span>
+        {!!Form::text('apellidos',null,['class'=>'form-control','placeholder'=>'Ingrese los apellidos'])!!}
+      </div>
+    <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-home margin10"></span>
+        {!!Form::text('direccion',null,['class'=>'form-control','placeholder'=>'Ingrese la dirección'])!!}
+      </div>
+    <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-globe"></span>
+        {!!Form::text('localidad',null,['class'=>'form-control','placeholder'=>'Ingrese la localidad'])!!}
+      </div>
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-calendar"></span>
+          {!!Form::date('fechanacimiento', \Carbon\Carbon::now(),['class'=>'form-control'])!!}
+      </div>
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-picture"></span>
+          {!!Form::file('imagen',['class'=>'form-control'])!!}
+      </div>
+      <div class="form-group margin10">
+        <span class="glyphicon glyphicon-new-window"> Hombre&nbsp</span>{!!Form::radio('genero', 'Hombre', true)!!}
+        <span class="glyphicon glyphicon-rub"> Mujer&nbsp </span>{!!Form::radio('genero', 'Mujer')!!}
+      </div>
+      {!!Form::submit('Registrar Usuario',['class'=>'btn boton'])!!}
+      {!!Form::close()!!}
+  </div>
+@endsection
