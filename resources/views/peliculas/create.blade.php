ada6cfc90f15c2166f33618173681bdf3cc96759
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-05-20 23:25:50 +0200

    peliculas create e index

---
+++ resources/views/peliculas/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+	@section('content')
+  {!!Form::open(['route'=>'adminpelicula.store','method'=>'POST'])!!}
+  <h1 class="textoMarron text-center">Formulario de registro de pelicula</h1>
+  <div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1">
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-envelope"></span>
+        {!!Form::text('titulo',null,['class'=>'form-control','placeholder'=>'Ingrese el titulo'])!!}
+      </div>
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-certificate"></span>
+        {!!Form::date('anio',null,['class'=>'form-control','placeholder'=>'Ingrese el año'])!!}
+      </div>
+       <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-font"></span>
+        {!!Form::text('pais',null,['class'=>'form-control','placeholder'=>'Ingrese el pais'])!!}
+      </div>
+    <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-bold"></span>
+        {!!Form::text('director',null,['class'=>'form-control','placeholder'=>'Ingrese el director'])!!}
+      </div>
+    <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-home margin10"></span>
+        {!!Form::text('guion',null,['class'=>'form-control','placeholder'=>'Ingrese guionistas'])!!}
+      </div>
+    <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-globe"></span>
+        {!!Form::text('reparto',null,['class'=>'form-control','placeholder'=>'Ingrese el reparto'])!!}
+      </div>
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-calendar"></span>
+          {!!Form::text('sinopsis', null,['class'=>'form-control','placeholder'=>'Ingrese la sinopsis'])!!}
+      </div>
+      <div class="input-group input-group-lg margin10">
+        <span class="input-group-addon glyphicon glyphicon-picture"></span>
+          {!!Form::file('imagen',['class'=>'form-control'])!!}
+      </div>
+      <div class="form-group margin10">
+        <span class="input-group-addon glyphicon glyphicon-picture"></span>
+          {!!Form::file('imagen',['class'=>'form-control'])!!}
+      </div>
+
+      {!!Form::submit('Registrar',['class'=>'btn boton'])!!}
+      {!!Form::close()!!}
+  </div>
+@endsection
---
+++ resources/views/peliculas/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.admin')
+	@section('content')
+
+  <?php $message=Session::get('message')?>
+	@if($message=='store')
+	<div class="alert alert-success alert-dismissible" role="alert">
+	  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	  <strong>Película Registrada</strong>
+	</div>
+	@endif
+<div class="container-fluid">
+<h1 class="text-center">Listado Peliculas</h1><br>
+	<table class="table table-hover text-center table-bordered">
+		<thead class="fondoMenu">
+        <th class="text-center textoBlanco">ID</th>
+        <th class="text-center textoBlanco">Título</th>
+        <th class="text-center textoBlanco">Año</th>
+        <th class="text-center textoBlanco hidden-xs">País</th>
+        <th class="text-center textoBlanco hidden-xs hidden-sm">Director</th>
+        <th class="text-center textoBlanco hidden-xs hidden-sm">Guión</th>
+        <th class="text-center textoBlanco hidden-xs">Reparto</th>
+        <th class="text-center textoBlanco hidden-xs">Sinopsis</th>
+        <th class="text-center textoBlanco hidden-xs hidden-sm">Trailer</th>
+        <th class="text-center textoBlanco">Cartelera</th>
+        <th class="text-center textoBlanco">Acciones</th>
+      </thead>
+			  @foreach($peliculas as $peliculas)
+      <tbody>
+        <td>{{$peliculas->id}}</td>
+        <td>{{$peliculas->titulo}}</td>
+        <td>{{$peliculas->anio}}</td>
+        <td class="hidden-xs">{{$peliculas->pais}}</td>
+        <td class="hidden-xs hidden-sm">{{$peliculas->director}}</td>
+        <td class="hidden-xs hidden-sm">{{$peliculas->guion}}</td>
+        <td class="hidden-xs">{{$peliculas->reparto}}</td>
+        <td class="hidden-xs">{{$peliculas->sinopsis}}</td>
+        <td class="hidden-xs hidden-sm">{{$peliculas->trailer}}</td>
+        <td>{{$peliculas->cartelera}}</td>
+        <td class="fila">
+          {!!link_to_route('adminpeliculas.edit', $title = "Editar", $parameters = $peliculas->id, $attributes = ['class'=>'btn boton2 margin5'])!!}
+          {!!Form::open(['route'=>['adminpeliculas.destroy',$pelicula->id],'method'=>'DELETE'])!!}
+					<div class='btn-group'>
+                    <a href="{!! route('peliculas.show', [$peliculas->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                    <a href="{!! route('peliculas.edit', [$peliculas->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                </div>
+          {!!Form::submit('Eliminar',['class'=>'btn btn-danger margin5'])!!}
+          {!!Form::close()!!}
+        </td>
+      </tbody>
+      @endforeach
+    </table>
+  </div>
+  @endsection
