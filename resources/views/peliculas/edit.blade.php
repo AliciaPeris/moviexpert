06a827d89e14f7608144d0be1341260b84304a64
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-05-31 19:07:20 +0200

    Mostrando los generos como un select - falta cambiar el css para que se vea bien

--- resources/views/peliculas/edit.blade.php
+++ resources/views/peliculas/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 	@section('content')
 
-  {!!Form::model($pelicula,['route'=>['adminpelicula.update',$pelicula->id],'method'=>'PUT'])!!}
+  {!!Form::model($pelicula,['route'=>['adminpelicula.update',$pelicula->id],'method'=>'PUT', 'files' => true])!!}
   <h1 class="textoMarron text-center">Editar pelicula</h1>
   <div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1">
       <div class="input-group input-group-lg margin10">
@@ -42,7 +42,7 @@
       </div>
       <div class="form-group input-group margin10">
         <span class="input-group-addon glyphicon glyphicon-home"></span>
-          {!!Form::text('genero',null,['class'=>'form-control','placeholder'=>'Ingrese el genero'])!!}
+          {!!Form::select('genero',$generos,["class"=>'form-control'])!!}
       </div>
 
       {!!Form::submit('Registrar',['class'=>'btn boton'])!!}
