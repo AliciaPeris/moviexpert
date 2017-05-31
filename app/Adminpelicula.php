ce1e5811b48a6a2e480d488bf20d23f3848faefa
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-05-31 19:05:05 +0200

    Intento de subida de fotos en las peliculas - Alicia mira que me puede faltar

--- app/Adminpelicula.php
+++ app/Adminpelicula.php
@@ -9,10 +9,15 @@ class Adminpelicula extends Model
     //
     //protected $table = 'adminpeliculas';
     protected $fillable = [
-        'titulo', 'anio', 'pais','director','guion','reparto','sinopsis','trailer','cartelera','genero',
-    ];
+        'titulo', 'anio', 'pais','director','guion','reparto','sinopsis','trailer','cartelersa','genero'];
+
+    public function setPathAttribute($cartelera){
+      $this->attributes['cartelera'] = Carbon::now()->second.$cartelera->getClientOriginalName();
+      $name =   Carbon::now()->second.$cartelera->getClientOriginalName();
+      \Storage::disk('local')->put($name, \File::get($cartelera)) ;
+    }
     public function genero()
    {
-       return $this->belongsTo('\App\Admingenero');
-   }
+       return $this->belongsTo('\App\Http\Admingenero');
+}
 }
