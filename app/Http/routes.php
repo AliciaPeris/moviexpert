32a6a3548bab2e736ba5f5ebf2d5669a917f705a
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-05 20:01:03 +0200

    vista de criticas sin funcionar el añadir criticas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,6 +23,7 @@ Route::resource('adminparticipanconcurso','AdminparticipanconcursoController');
 /*Rutas Frontend*/
 Route::resource('/','FrontendController');
 Route::get('peliculas','FrontendController@peliculas');
+Route::get('criticas/{id}','FrontendController@criticas');
 Route::get('ficha/{id}','FrontendController@ficha');
 Route::get('concursos','FrontendController@concursos');
 Route::get('chat','FrontendController@chat');
