08f8701e9cc7719fae8247e6c35b3c8ba4a85e4b
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-05 18:07:24 +0200

    añadir vista de peliculas

--- resources/views/frontend/peliculas.blade.php
+++ resources/views/frontend/peliculas.blade.php
@@ -1,5 +1,29 @@
-@extends('layouts.frontend')
+<?php use moviexpert\Http\Controllers\FrontendController;
+	?>
+	@extends('layouts.frontend')
 	@section('content')
+
+	<div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 cuadrado">
+		<ul class="nav nav-tabs">
+			<li class=""><a class="textoFicha" href="/">Vista general</a></li>
+			<li class=""><a class="textoFicha" href="/peliculas">Valoración</a></li>
+		</ul>
+		<div class="col-xs-12 col-md-12">
+			<div class="contenedor col-xs-12 col-md-6">
+				@foreach($peliculas as $pelicula)
+					<a class="fichaRes2" href="{{ url('/ficha/') }}/{{$pelicula->id}}">
+					<img class="marginBotton" src="/imagenes/cartelera/{{$pelicula->cartelera}}">
+
+		        <div class="colorNegro marginBotton"> <span class="titulosPeliculas">Título: </span> {{$pelicula->titulo}}</div>
+		        <div class="colorNegro marginBotton"> <span class="titulosPeliculas">Año: </span>{{$pelicula->anio}}</div>
+						<div class="colorNegro marginBotton"><span class="titulosPeliculas">Director: </span> {{$pelicula->director}}</div>
+						<div class="colorNegro marginBotton"><span class="titulosPeliculas">Sinopsis: </span>{{$pelicula->sinopsis}}</div>
+					</a>
+				</div>
+				@endforeach
+			</div>
+		</div>
+
 
 
 @endsection
