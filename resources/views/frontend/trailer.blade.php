8520c446409398805f748051a3a9ecbd34b321d4
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-06 23:19:13 +0200

    he cambiado la vista de trailer para que salgan menos campos

--- resources/views/frontend/trailer.blade.php
+++ resources/views/frontend/trailer.blade.php
@@ -5,20 +5,18 @@
 
   <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 cuadrado">
   		<ul class="nav nav-tabs">
-        <li class=""><a class="textoFicha" href="{{ url('/ficha/') }}/{{$peliculas->id}}">Ficha</a></li>
-        <li class=""><a class="textoFicha" href="{{ url('/trailer/') }}/{{$peliculas->id}}">Trailer</a></li>
-        <li class=""><a class="textoFicha" href="{{ url('/criticas/') }}/{{$peliculas->id}}">Críticas</a></li>
+        <li class=""><a class="textoFicha" href="{{ url('/ficha/') }}/{{$pelicula->id}}">Ficha</a></li>
+        <li class=""><a class="textoFicha" href="{{ url('/trailer/') }}/{{$pelicula->id}}">Trailer</a></li>
+        <li class=""><a class="textoFicha" href="{{ url('/criticas/') }}/{{$pelicula->id}}">Críticas</a></li>
       </ul>
       <div id="fichaPeli">
         <div class="col-xs-5">
-        <img class="fotoTrailer" src="/imagenes/cartelera/{{$peliculas->cartelera}}" alt="{{$peliculas->titulo}}">
+        <img class="fotoTrailer" src="/imagenes/cartelera/{{$pelicula->cartelera}}" alt="{{$pelicula->titulo}}">
       </div>
       <div class="col-xs-6 col-offset-4">
-  	        <div class="marginBotton  marginLeft"><span class="titulosPeliculas">Título: </span>  {{$peliculas->titulo}}</div>
-  	        <div class="marginBotton marginLeft"><span class="titulosPeliculas"> Año: </span> {{$peliculas->anio}}</div>
-  					<div class="marginBotton marginLeft"><span class="titulosPeliculas"> Sinopsis: </span> {{$peliculas->sinopsis}}</div>
-  					<div class="marginBotton marginLeft"><span class="titulosPeliculas"> Género:</span> {{$peliculas->genero}}</div>
-            <iframe id="trailer" src="https://www.youtube.com/embed/aDI21FZaecM" frameborder="0" allowfullscreen></iframe>
+  	        <div class="marginBotton  marginLeft"><span class="titulosPeliculas">Título: </span>  {{$pelicula->titulo}}</div>
+  	        <div class="marginBotton marginLeft"><span class="titulosPeliculas"> Año: </span> {{$pelicula->anio}}</div>
+  				<iframe id="trailer" src="https://www.youtube.com/embed/aDI21FZaecM" frameborder="0" allowfullscreen></iframe>
 
       </div>
   		</div>
