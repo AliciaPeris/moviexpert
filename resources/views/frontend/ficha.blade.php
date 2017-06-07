a60533b59ab0f51ec248d79431fff32da676c0b8
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-07 13:43:53 +0200

    cambiado el diseño de ficha

--- resources/views/frontend/ficha.blade.php
+++ resources/views/frontend/ficha.blade.php
@@ -2,21 +2,23 @@
 @section('content')
 <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 cuadrado">
 		<ul class="nav nav-tabs">
-			<li class=""><a class="textoFicha" href="">Ficha</a></li>
+			<li class="active"><a class="textoFicha" href="">Ficha</a></li>
 			<li class=""><a class="textoFicha" href="{{ url('/trailer/') }}/{{$pelicula->id}}">Trailer</a></li>
 			<li class=""><a class="textoFicha" href="{{ url('/criticas/') }}/{{$pelicula->id}}">Críticas</a></li>
 
 		</ul>
-		<div id="fichaPeli">
-	        <div class="tituloP text-center">Título: {{$pelicula->titulo}}</div>
-	        <div class="anioP text-center"> Año: {{$pelicula->anio}}</div>
-					<div class="anioP text-center"> País: {{$pelicula->pais}}</div>
-					<div class="anioP text-center"> Director: {{$pelicula->director}}</div>
-					<div class="anioP text-center"> Guión: {{$pelicula->guion}}</div>
-					<div class="anioP text-center"> Reparto: {{$pelicula->reparto}}</div>
-					<div class="anioP text-center"> Sinopsis: {{$pelicula->sinopsis}}</div>
-					<div class="anioP text-center"> Género: {{$generos[$pelicula->genero]}}</div>
-				<img id="fotoFicha" src="/imagenes/cartelera/{{$pelicula->cartelera}}" alt="{{$pelicula->titulo}}">
+		<div id="fichaPeli col-xs-12">
+				<img id="fotoFicha" class="col-xs-4 col-md-4" src="/imagenes/cartelera/{{$pelicula->cartelera}}" alt="{{$pelicula->titulo}}">
+
+	        <div class="marginBottonFicha"><span class="titulosPeliculas">Título: </span>{{$pelicula->titulo}}</div>
+	        <div class="marginBottonFicha"><span class="titulosPeliculas"> Año: </span>{{$pelicula->anio}}</div>
+					<div class="marginBottonFicha"> <span class="titulosPeliculas">País: </span> {{$pelicula->pais}}</div>
+					<div class="marginBottonFicha"><span class="titulosPeliculas">Director:</span> {{$pelicula->director}}</div>
+					<div class="marginBottonFicha"><span class="titulosPeliculas">Guión:</span> {{$pelicula->guion}}</div>
+					<div class="marginBottonFicha"><span class="titulosPeliculas">Reparto: </span>{{$pelicula->reparto}}</div>
+					<div class="marginBottonFicha"><span class="titulosPeliculas">Género: </span>{{$generos[$pelicula->genero]}}</div>
+					<div class="marginBottonFicha"><span class="titulosPeliculas">Sinopsis: </span>{{$pelicula->sinopsis}}</div>
+
 		</div>
 </div>
 
