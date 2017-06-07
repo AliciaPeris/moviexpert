d8f817f4569094aa690bfbc556c89ca0d5e25dea
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-07 12:56:53 +0200

    responsive la vista de trailer

--- resources/views/frontend/trailer.blade.php
+++ resources/views/frontend/trailer.blade.php
@@ -9,11 +9,11 @@
         <li class=""><a class="textoFicha" href="{{ url('/trailer/') }}/{{$pelicula->id}}">Trailer</a></li>
         <li class=""><a class="textoFicha" href="{{ url('/criticas/') }}/{{$pelicula->id}}">Críticas</a></li>
       </ul>
-      <div id="fichaPeli">
-        <div class="col-xs-5">
-        <img class="fotoTrailer" src="/imagenes/cartelera/{{$pelicula->cartelera}}" alt="{{$pelicula->titulo}}">
+      <div id="fichaPeli col-xs-12 col-md-6">
+        <div class="col-xs-12 col-md-6">
+        <img class="fotoTrailer col-xs-6" src="/imagenes/cartelera/{{$pelicula->cartelera}}" alt="{{$pelicula->titulo}}">
       </div>
-      <div class="col-xs-6 col-offset-4">
+      <div class="col-xs-12 col-md-6 col-offset-4">
   	        <div class="marginBotton  marginLeft"><span class="titulosPeliculas">Título: </span>  {{$pelicula->titulo}}</div>
   	        <div class="marginBotton marginLeft"><span class="titulosPeliculas"> Año: </span> {{$pelicula->anio}}</div>
 
