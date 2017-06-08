14b69ee39d48af71298d201e5ae21e85c864ba9e
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-08 16:55:22 +0200

    mejora de la vista de criticas y del botón de borrar cuenta

--- resources/views/frontend/criticas.blade.php
+++ resources/views/frontend/criticas.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.frontend')
 	@section('content')
-<div class="col-xs-12 cuadrado container-fluid margintop25">
-  <h1 class="col-xs-12 text-center">Críticas</h1>
+
+
 
   <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 cuadrado">
+		  <h1 class="col-xs-12 text-center">Críticas</h1>
   		<ul class="nav nav-tabs">
         <li class=""><a class="textoFicha" href="{{ url('/ficha/') }}/{{$peliculas->id}}">Ficha</a></li>
-        <li class=""><a class="textoFicha" href="/peliculas">Trailer</a></li>
+        <li class=""><a class="textoFicha" href="{{ url('/trailer/') }}/{{$peliculas->id}}">Trailer</a></li>
         <li class="active"><a class="textoFicha" href="{{ url('/criticas/') }}/{{$peliculas->id}}">Críticas</a></li>
       </ul>
       <a class='btn boton col-xs-12 col-md-3 margin10' href="{{url('criticas/')}}">Añadir Crítica</a>
-</div>
+
 
   @endsection
