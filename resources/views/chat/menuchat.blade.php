edbe5fb88625c1a1ea8a70aa7e680845c41ca585
AliciaPeris <aliciadelacruzperis@gmail.com>
2017-06-10 10:46:35 +0200

    Crear controladores para el backend

--- resources/views/chat/menuchat.blade.php
+++ resources/views/chat/menuchat.blade.php
@@ -1,7 +1,8 @@
 
 <div class="col-xs-12 text-center cuadrado">
   <p class="col-xs-12 textoMarron"> Actor: <i class="glyphicon glyphicon-user"></i> | Guionista: <i class="glyphicon glyphicon-list-alt"></i> | Director: <i class="glyphicon glyphicon-film"></i> | Camara: <i class="glyphicon glyphicon-facetime-video"></i></p>
-  <div class="margintopbottom25">
+</div>
+<div class="margintopbottom25">
     <a class="btn boton margin5" href="/chat">Lista Chat</a>
 {!!link_to_route('miembrochat.index', $title = "Mis Chat",$parameters= "", $attributes = ['class'=>'btn boton margin5'])!!}
 {!!link_to_route('grupochat.index', $title = "Administrar Grupo Chat",$parameters= "", $attributes = ['class'=>'btn boton margin5'])!!}
