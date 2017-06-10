bf7504523ca00d4a8076ec8345e137a816b3ba99
AliciaPeris <aliciadelacruzperis@gmail.com>
2017-06-10 10:52:48 +0200

    Chat

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -3,7 +3,7 @@
 	@section('content')
 	@include('chat.menuchat')
   @foreach($chats as $chat)
-<div class="col-xs-12 col-sm-6 col-md-4 col-md-offset-1 cuadrado ">
+<div class="col-xs-12 col-sm-6 col-md-4 col-md-offset-1 cuadrado margintop25">
 	<h1 class="col-xs-12 text-center">{{$chat->nombre}}</h1>
 	<p class="col-xs-12 text-center">Administrador del Grupo: {{$chat->creadorchat}}</p>
 	<p class="col-xs-12">{{$chat->descripcion}}</p>
