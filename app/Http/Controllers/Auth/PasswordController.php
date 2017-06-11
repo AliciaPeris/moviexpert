f7747fe885c7fb285b40b9077a3e44d3dd2cd01b
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-11 15:36:15 +0200

    arreglado el problema de resetear contraseña

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -22,11 +22,15 @@ class PasswordController extends Controller
 
     /**
      * Create a new password controller instance.
+      
      *
      * @return void
      */
     public function __construct()
     {
+
         $this->middleware('guest');
+
+
     }
 }
