74d9376d0447689765733a3a35ebe86cd525f723
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-13 16:31:35 +0200

    placeholders buscadores

--- resources/views/concursos/index.blade.php
+++ resources/views/concursos/index.blade.php
@@ -13,7 +13,7 @@
 	<form class "navbar-form navbar-left col-xs-12" role="search" method="POST" action="/buscarconcursos">
 		{{ csrf_field() }}
 		<div class="form-group col-xs-6 col-md-2">
-			<input type="text" name="nombre" class="form-control" placeholder="Buscar">
+			<input type="text" name="nombre" class="form-control" placeholder="Buscar por nombre">
 		</div>
 		<button type="submit" class="btn btn-danger col-xs-3 col-md-1">Buscar</button>
 	</form>
