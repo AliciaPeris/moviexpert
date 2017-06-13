4c298ac8eb2b69f18e6ffe9c41e0bfe445ddf389
sheila89 <sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.comgit config --global user.email sheila_bravo@hotmail.com>
2017-06-13 18:33:13 +0200

    mejorada vista de chat

--- resources/views/frontend/chat.blade.php
+++ resources/views/frontend/chat.blade.php
@@ -10,7 +10,8 @@
 		$nombre=UserController::nombreUser($iduser);
 	?>
 	@foreach($nombre as $nom)
-<div class="col-xs-12 col-sm-6 col-md-4 col-md-offset-1 cuadrado">
+<div class="col-xs-12 col-sm-6 col-md-4 col-md-offset-1 cuadrado chats">
+
 	<h1 class="col-xs-12 text-center">{{$chat->nombre}}</h1>
 	<p class="col-xs-12 text-center">Administrador del Grupo: {{$chat->creadorchat}} <?php echo " - ".$nom->nombre;?></p>
 	<p class="col-xs-12">{{$chat->descripcion}}</p>
