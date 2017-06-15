2b75544cbb5af96d71ce3d3259aa69b24629be58
AliciaPeris <aliciadelacruzperis@gmail.com>
2017-06-15 11:00:45 +0200

    Cambiar iconos de todas las opciones del menu concursos del backend

--- resources/views/votosconcursos/edit.blade.php
+++ resources/views/votosconcursos/edit.blade.php
@@ -6,19 +6,19 @@
 	<div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 blanco">
 		  <h1 class="textoMarron text-center">Editar Voto</h1>
       <div class="input-group input-group-lg margin10">
-        <span class="input-group-addon glyphicon glyphicon-envelope"></span>
+        <span class="input-group-addon glyphicon glyphicon-certificate"></span>
         {!!Form::select('idcortoconcurso',$cortos,null,["class"=>'form-control'])!!}
       </div>
-			<div class="form-group input-group-lg margin10">
+			<div class="input-group input-group-lg margin10">
         <span class="input-group-addon glyphicon glyphicon-user"></span>
           {!!Form::select('idusuario',$usuarios,null,["class"=>'form-control'])!!}
       </div>
       <div class="input-group input-group-lg margin10">
-        <span class="input-group-addon glyphicon glyphicon-envelope"></span>
+        <span class="input-group-addon glyphicon glyphicon-star"></span>
         {!!Form::number('voto',null,['class'=>'form-control','placeholder'=>'Ingrese la fecha de inicio'])!!}
       </div>
       <div class="input-group input-group-lg margin10">
-        <span class="input-group-addon glyphicon glyphicon-envelope"></span>
+        <span class="input-group-addon glyphicon glyphicon-calendar"></span>
         {!!Form::date('fechavoto',null,['class'=>'form-control','placeholder'=>'Ingrese la fecha fin de concurso'])!!}
       </div>
       {!!Form::submit('Registrar',['class'=>'btn boton'])!!}
